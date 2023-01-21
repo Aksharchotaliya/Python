c432ca77f1a449c1a0ec800a1d1bdb154cd77c40
Akshar chotaliya <99075130+Aksharchotaliya@users.noreply.github.com>
2023-01-21 12:33:02 +0530

    Update array_functions.py

--- array_functions.py
+++ array_functions.py
@@ -2,26 +2,31 @@ import array as arr
 
 numbers = arr.array('i',[10,20,30])
 
+#this len() is used to find length of the array:
 print("this len() is used to find length of the array:")
 print(len(numbers))
 
+#arry start with 0 index value:
 print("arry start with 0 index value:")
 print(numbers[0])
 print(numbers[1])
 print(numbers[2])
-
+#the position of the array is n -1
 print("the position of the array is n -1")
 print(numbers[-1])
 print(numbers[-2])
 print(numbers[-3])
 
+#this index() is used to find position of the array:
 print("this index() is used to find position of the array:")
 print(numbers.index(10))
 
+#using for loop you can print the element without writeing individually:
 print("using for loop you can print the element without writeing individually:")
 for numbers in numbers:
     print(numbers)
 
+#the range() function and pass the len() method as its parameter This would give the same result as above:
 print("the range() function and pass the len() method as its parameter This would give the same result as above:")    
 for value in range(len(numbers)):
     print(numbers[value])
